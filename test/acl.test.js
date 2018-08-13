abb83e653e3a5b0ea8fb7ab4dde6a15e30cfc34c
jannyHou <juehou@ca.ibm.com>
2018-08-13 13:36:00 -0400

    update relations generator

--- test/acl.test.js
+++ test/acl.test.js
@@ -13,8 +13,6 @@ var SANDBOX = path.resolve(__dirname, 'sandbox');
 var expect = require('chai').expect;
 var common = require('./common');
 
-// Added `only` in the first commit since it updates the acl generator only
-// Will remove it in the following commit
 describe('loopback:acl generator', function() {
   beforeEach(function createSandbox(done) {
     helpers.testDirectory(SANDBOX, done);
